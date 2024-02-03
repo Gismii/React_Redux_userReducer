c9819ce81467f9db9d712ca04a298ea60ef86f37
Gismii <gisme.guimaraes@gmail.com>
2024-02-03 00:37:09 -0300

    adiciona redux-logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
-import { createStore } from "redux";
-
+import { createStore, applyMiddleware } from "redux";
+//aplyMiddlware serve para exibir os logs, olpcional.
 import rootReducer from './root-reduce';
+import logger from "redux-logger";
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 export default store;
